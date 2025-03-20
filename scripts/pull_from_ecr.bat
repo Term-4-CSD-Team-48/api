d3f17355d7684e33fa6a4a9cf67a3430b31cd723
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2025-03-20 20:13:56 +0800

    refactor: moved custom scripts to scripts folder

---
+++ scripts/pull_from_ecr.bat
@@ -0,0 +1,4 @@
+echo Pulling Docker image from public.ecr.aws/l1g2c1s4/50.001:api
+docker pull public.ecr.aws/l1g2c1s4/50.001:api
+
+echo Docker image pulled successfully!
---
+++ scripts/run_docker_ecr.bat
@@ -0,0 +1,1 @@
+docker run --env-file .env -it --rm -p 8080:8080 -p 5000:5000/udp public.ecr.aws/l1g2c1s4/50.001:api
