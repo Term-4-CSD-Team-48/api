e4777db869397c9551a66738f05be7a382d470e3
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2025-03-24 20:44:25 +0800

    test: removed testing by UDP packets as API now no longer needs to send any UDP packets

--- src/test/java/com/term_4_csd__50_001/api/AiControllerTest.java
+++ src/test/java/com/term_4_csd__50_001/api/AiControllerTest.java
@@ -49,37 +49,6 @@ public class AiControllerTest extends BaseTest {
                  * .exchange().expectStatus().isOk();
                  */
 
-                // CRITICAL: The following test will never throw an error. Please check
-                // console to verify that UDP has been sent and received.
-                // One way is to stop all other tests from running or someone
-                // please finally make this test testable.
-                webTestClient.post().uri(subscribeEP).cookie(jSessionIdCookieName, jSessionId)
-                                .exchange().expectStatus().isOk();
-                String frameData = "frame_data";
-                byte[] frameDataBytes = frameData.getBytes();
-                byte[] payload = new byte[sharedSecretBytes.length + frameDataBytes.length];
-                System.arraycopy(sharedSecretBytes, 0, payload, 0, sharedSecretBytes.length);
-                System.arraycopy(frameDataBytes, 0, payload, sharedSecretBytes.length,
-                                frameDataBytes.length);
-                DatagramSocket socket = new DatagramSocket();
-                InetAddress address = InetAddress.getByName("localhost");
-                int port = 5000;
-                for (int i = 0; i < 10; i++) {
-                        frameData = "frame_data" + i;
-                        frameDataBytes = frameData.getBytes();
-                        payload = new byte[sharedSecretBytes.length + frameDataBytes.length];
-                        System.arraycopy(sharedSecretBytes, 0, payload, 0,
-                                        sharedSecretBytes.length);
-                        System.arraycopy(frameDataBytes, 0, payload, sharedSecretBytes.length,
-                                        frameDataBytes.length);
-                        DatagramPacket packet =
-                                        new DatagramPacket(payload, payload.length, address, port);
-                        Thread.sleep(100);
-                        socket.send(packet);
-                        System.out.println("Sent UDP packet " + frameData + " to " + address + ":"
-                                        + port + " at " + LocalDateTime.now());
-                }
-                socket.close();
         }
 
 }
