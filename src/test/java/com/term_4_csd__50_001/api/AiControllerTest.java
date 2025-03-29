923f07815c58ce4f8c798a0a6a7d43a6c561c4d6
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2025-03-29 15:45:03 +0800

    test: added tests for inovcationEP and assumeControlEP

--- src/test/java/com/term_4_csd__50_001/api/AiControllerTest.java
+++ src/test/java/com/term_4_csd__50_001/api/AiControllerTest.java
@@ -12,13 +12,22 @@ public class AiControllerTest extends BaseTest {
         private AuthService authService;
 
         @Test
-        public void subscribeTest() throws Exception {
+        public void invocationsTest() throws Exception {
                 String emailFieldName = "email";
                 String passwordFieldName = "password";
                 String email = "xyf.oco@gmail.com";
                 String username = "username";
                 String rawPassword = "password";
                 authService.register(email, username, rawPassword, true);
+
+                // Should throw 401
+                webTestClient.post().uri(invocationsEP).contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue("{\"x\": 12.3, \"y\": 45.6}").exchange().expectStatus()
+                                .isUnauthorized();
+
+                // Should throw 401
+                webTestClient.post().uri(assumeControlEP).exchange().expectStatus()
+                                .isUnauthorized();
 
                 // Login with correct credentials
                 HttpHeaders headers = webTestClient.post().uri(loginEP)
@@ -33,17 +42,15 @@ public class AiControllerTest extends BaseTest {
                 String jSessionId = extractJSessionIdFromSetCookieHeader(setCookieHeader);
                 System.out.println("Extracted JSESSIONID: " + jSessionId);
 
-                // Register camera
-                // Not needed to start listening as CameraController doesn't accept /start-listening
-                // requests and CameraServices will automatically ping the camera to listen
-                /**
-                 * final String cameraSecret = dotenv.get(Dotenv.CAMERA_SECRET);
-                 * webTestClient.post().uri(cameraStartListeningEP)
-                 * .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                 * .bodyValue(cameraSecretFieldName + "=" + cameraSecret + "&" +
-                 * sharedSecretFieldName + "=" + urlEncodedSharedSecret)
-                 * .exchange().expectStatus().isOk();
-                 */
+                // Should return 503 (test is meant to run without AI server)
+                webTestClient.post().uri(assumeControlEP).cookie(jSessionIdCookieName, jSessionId)
+                                .exchange().expectStatus().isEqualTo(503);
+
+                // Should throw 503 (test is meant to run without AI server)
+                webTestClient.post().uri(invocationsEP).contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue("{\"x\": 12.3, \"y\": 45.6}")
+                                .cookie(jSessionIdCookieName, jSessionId).exchange().expectStatus()
+                                .isEqualTo(503);
 
         }
 
