46b22c0f02ae7716ed70ae198ce13ed5f411d697
XlightNtrEnx <51813601+XlightNtrEnx@users.noreply.github.com>
2025-04-03 13:16:32 +0800

    Delete scripts directory

--- scripts/pull_from_ecr.bat
+++
@@ -1,4 +0,0 @@
-echo Pulling Docker image from public.ecr.aws/l1g2c1s4/50.001:api
-docker pull public.ecr.aws/l1g2c1s4/50.001:api
-
-echo Docker image pulled successfully!
--- scripts/run_docker_ecr.bat
+++
@@ -1,1 +0,0 @@
-docker run --env-file .env -it --rm -p 8080:8080 -p 5000:5000/udp public.ecr.aws/l1g2c1s4/50.001:api
